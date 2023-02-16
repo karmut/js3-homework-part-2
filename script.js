a7f1c97bf20c3d8f944996fc017cae45cd757ae8
karmut <karina.mutafyan@gmail.com>
2023-02-16 18:41:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let n1 = prompt();
+let n2 = prompt();
+
+let number1 = Number(n1); // string->number
+let number2 = Number(n2); // string->number
+
+for (let i = 0; i <= n2; i++) {
+  console.log(i);
+}
